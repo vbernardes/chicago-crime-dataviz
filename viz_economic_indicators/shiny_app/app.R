81f1e705fee2a7a81733788becb77899a9733c76
Vitor Sexto Bernardes <vitorsb@gmail.com>
2019-12-12 17:01:59 +0000

    Link plots using crosstalk

--- viz_economic_indicators/shiny_app/app.R
+++ viz_economic_indicators/shiny_app/app.R
@@ -1,17 +1,16 @@
 # Setup environment ----
 library(shiny)
-library(SDMTools)
 library(plotly)
+library(crosstalk)
 
 # Load data & resources ----
 eco_indicators_df <- read.csv('data/Economic Indicators and Crime 2011.csv')
-source('draw_map.R')
 
 # Helper functions ----
-scatter_indicators <- function(x,
+scatter_indicators <- function(shared_df,
+                               x,
                                y = 'Crimes.per.Capita',
-                               color_by = 'Proportion.of.Violent.Crimes',
-                               highlight) {
+                               color_by = 'Proportion.of.Violent.Crimes') {
   # set plot labels
   y_label <- 'Crimes per Capita'
   x_label <- switch (x,
@@ -23,46 +22,18 @@ scatter_indicators <- function(x,
                          'Proportion.of.Violent.Crimes' = 'Proportion of\nViolent Crimes',
                          'Proportion.of.Arrests' = 'Proportion\nof Arrests')
   
-  main_scatter <- plot_ly(eco_indicators_df,
+  main_scatter <- plot_ly(shared_df,
                           x=~get(x), y=~get(y),
                           color=~get(color_by),
                           # name = guide_title,
                           colors='Reds',
                           hoverinfo = 'text',
-                          text = ~paste(Community.Area.Name)) %>%
+                          text = ~paste(Community.Area.Name),
+                          source = 'scatter') %>%
+    add_trace(type='scatter') %>%
     layout(xaxis = list(title=x_label, zeroline=FALSE, fixedrange=TRUE),
            yaxis = list(title=y_label, zeroline=FALSE, fixedrange=TRUE)) %>%
     config(displayModeBar = F)
-  
-  if (nrow(highlight) == 0) {
-    main_scatter
-  }
-  else {
-    # highlight specific point
-    main_scatter +
-      geom_point(data = subset(eco_indicators_df,
-                               Community.Area == highlight$Community.Area),
-                 aes_string(x, y),
-                 shape = 21,
-                 alpha = 0.2,
-                 fill = 'red',
-                 # color = 'red',
-                 size = 9) +
-      guides(fill=FALSE) +
-      scale_fill_continuous(low='white', high='red')
-  }
-}
-
-point_in_comm <- function(point_coords) {
-  # return community ID of point in point_coords.
-  for (id in seq(1,77)) {
-    comm_boundaries <- fortified_comms[fortified_comms$id==id, 1:2]
-    if (pnt.in.poly(point_coords, comm_boundaries)$pip) {
-      return(as.numeric(id))
-    }
-  }
-  # if we leave the loop and don't find a community, return 0
-  return(0)
 }
 
 
@@ -71,7 +42,7 @@ ui <- fluidPage(
   style = 'padding: 5%; font-size: small;',
   fluidRow(
     h1('Crime and Poverty in Chicago'),
-    p('People in poorer communities not only have to struggle with economic
+    p('People in poor communities not only have to struggle with economic
       difficulties, but are also more exposed to crime. The plot below presents
       a snapshot of Chicago in 2011, and shows a strong association between the
       socioeconomic status of a community and crime activity. Each point
@@ -110,36 +81,23 @@ ui <- fluidPage(
 
 # Server logic ----
 server <- function(input, output, session) {
-  highlighted_comm <- reactiveVal(data.frame()) # initialize w/ empty data frame
+  
+  # create shared data frame for all plots
+  merged_shape <- merge(comm_shape, eco_indicators_df,
+                        by.x='area_numbe',
+                        by.y='Community.Area')
+  shared_df <- SharedData$new(merged_shape)
+  # load function to draw map
+  source('draw_map.R')
   
   output$plot <- renderPlotly({
-    scatter_indicators(x = input$indicator,
-                       color_by = input$color_by,
-                       highlight = highlighted_comm())
-  })
-  
-  observeEvent(input$scatter_click, {
-    highlighted_comm(nearPoints(eco_indicators_df,
-                               input$scatter_click,
-                               maxpoints = 1))
-  })
-  
-  observeEvent(input$map_click, {
-    clicked_coords <- data.frame(input$map_click[1], input$map_click[2])
-    id <- point_in_comm(clicked_coords)
-    if (id == 0) {
-      highlighted_comm(data.frame())
-    }
-    else {
-      highlighted_comm(eco_indicators_df[id,]) 
-    }
+    scatter_indicators(shared_df,
+                       x = input$indicator,
+                       color_by = input$color_by)
   })
   
   output$map <- renderPlotly({
-    # read hover data
-    # eventdata <- event_data("plotly_hover", source = "map")
-    draw_comm_areas(highlighted_comm(),
-                    input$indicator)
+    draw_comm_areas(shared_df)
   })
 }
 
--- viz_economic_indicators/shiny_app/draw_map.R
+++ viz_economic_indicators/shiny_app/draw_map.R
@@ -5,21 +5,11 @@ library(ggplot2)
 # Read Community Areas coordinates
 comm_shape <- read_sf(dsn = 'data/Boundaries - Community Areas (current)',
                       layer = 'geo_export_5b358314-1bdf-449f-84dd-bb266d9a1459')
-# shape_merc <- st_transform(comm_shape, 3857)
-# simple_shape <- st_simplify(shape_merc, preserveTopology = TRUE, dTolerance = 1000)
-# fortified_comms <- tidy(comm_shape, region = 'area_numbe')
-# add community name to fortified_comms
-# fortified_comms <- merge(fortified_comms,
-#                          comm_shape@data[,c("area_numbe", "community")],
-#                          by.x='id',
-#                          by.y='area_numbe')
 
 
-draw_comm_areas <- function(highlight_comm, indicator) {
-  # this function receives a data.frame with 0 or 1 row
-  
+draw_comm_areas <- function(shared_df) {
   # main map with no highlighted communities
-  main_plot <- plot_ly(comm_shape,
+  main_plot <- plot_ly(shared_df,
                        split = ~community,
                        line = list(width = 1),
                        fillcolor = '#FFDED4',
@@ -32,19 +22,4 @@ draw_comm_areas <- function(highlight_comm, indicator) {
                        source = 'map',
                        showlegend = FALSE) %>%
     config(displayModeBar = F)
-  
-  if (nrow(highlight_comm) == 0) {
-    main_plot
-  }
-  else {
-    comm_id <- highlight_comm$Community.Area
-    # plot highlighted community
-    # main_plot +
-    #   geom_polygon(aes(x = long,
-    #                    y = lat,
-    #                    group = group),
-    #                    fill = '#FF8363',
-    #                data = fortified_comms[fortified_comms$id == comm_id,]) +
-    #   guides(fill=FALSE)
-  }
 }
